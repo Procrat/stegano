ff7eae7a36332fe88584281e3424e55d916458ac
Stijn Seghers <stijnseghers@gmail.com>
2023-04-20 09:06:10 +0200

    png: Parse integers consistently

--- png.py
+++ png.py
@@ -27,8 +27,8 @@ def check_normal_format(data):
                 # print(f'sRGB chunk: sRGB colour space (rendering intent {chunk_data[0]}).')
             case b'pHYs':
                 assert len(chunk_data) == 9
-                ppu_x = int.from_bytes(chunk_data[:4], 'big')
-                ppu_y = int.from_bytes(chunk_data[4:8], 'big')
+                ppu_x = parse_int(chunk_data[:4])
+                ppu_y = parse_int(chunk_data[4:8])
                 unit = chunk_data[8]
                 assert unit == 1  # Metres
                 # print(f'pHYs chunk: Pixels per meter: {ppu_x}x{ppu_y}.')
@@ -39,7 +39,7 @@ def check_normal_format(data):
                 print(f'iTXt chunk: {chunk_data[22:].decode()}')
             case b'gAMA':
                 assert len(chunk_data) == 4
-                # gamma = int.from_bytes(chunk_data, 'big')
+                # gamma = parse_int(chunk_data)
                 # print(f'gAMA chunk: Gamma: {gamma}')
             case b'cHRM':
                 # print('cHRM chunk.')
@@ -63,7 +63,7 @@ def check_normal_format(data):
 
 
 def parse_chunk(data, offset):
-    length = int.from_bytes(data[offset:offset + 4], 'big')
+    length = parse_int(data[offset:offset + 4])
     chunk_type = data[offset + 4:offset + 8]
     chunk_data = data[offset + 8:offset + 8 + length]
     # NB: data may be hidden in CRC. We ignore it here.
@@ -71,11 +71,11 @@ def parse_chunk(data, offset):
 
 
 def parse_ihdr_data(data):
-    width = int.from_bytes(data[:4], 'big')
-    height = int.from_bytes(data[4:8], 'big')
-    bit_depth = int.from_bytes(data[8:9], 'big')
+    width = parse_int(data[:4])
+    height = parse_int(data[4:8])
+    bit_depth = parse_int(data[8:9])
     assert bit_depth in (1, 2, 4, 8, 16)
-    colour_type = int.from_bytes(data[9:10], 'big')
+    colour_type = parse_int(data[9:10])
     assert colour_type in (0, 2, 3, 4, 6)
     colour_type_str = {
         0: 'greyscale',
@@ -85,11 +85,15 @@ def parse_ihdr_data(data):
         6: 'truecolour with alpha',
     }[colour_type]
     print(f'Dimensions: {width}x{height}, {bit_depth}-bit, {colour_type_str}')
-    compression_method = int.from_bytes(data[10:11], 'big')
+    compression_method = parse_int(data[10:11])
     assert compression_method == 0
-    filter_method = int.from_bytes(data[11:12], 'big')
+    filter_method = parse_int(data[11:12])
     assert filter_method == 0
-    interlace_method = int.from_bytes(data[13:14], 'big')
+    interlace_method = parse_int(data[13:14])
     assert interlace_method == 0
     assert len(data) == 13
     return width, height, bit_depth
+
+
+def parse_int(data):
+    return int.from_bytes(data, 'big')
